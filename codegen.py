e9120fd69d1b6c62ef52ee5ec71814c4406bf53f
zhang-yang <dimlight1998@gmail.com>
2021-01-01 16:29:19 +0800

    more compact code

--- codegen.py
+++ codegen.py
@@ -26,7 +26,40 @@ def case_gen(args):
     return outputs[:-1]
 
 def case_gen_compact(args):
-    pass
+    def build_tree(d):
+        return None if d == 0 else {True: build_tree(d - 1), False: build_tree(d - 1)}
+    def fill_tree(tree, choices):
+        current_choices = choices[: :]
+        if tree[True] == None and tree[False] == None:
+            tree[True] = get_actions(current_choices + [True])
+            tree[False] = get_actions(current_choices + [False])
+        else:
+            fill_tree(tree[True], choices + [True])
+            fill_tree(tree[False], choices + [False])
+    def consolidate_tree(tree):
+        if type(tree) == type({}):
+            tree[True] = consolidate_tree(tree[True])
+            tree[False] = consolidate_tree(tree[False])
+            if tree[True] == tree[False]:
+                return tree[True]
+            else:
+                return {True: tree[True], False: tree[False]}
+        else:
+            return tree
+
+    branches, always = args
+    def get_actions(choices):
+        actions = set(always)
+        for i, choice in enumerate(choices):
+            if choice: [actions.add(action) for action in branches[i][1]]
+        return actions
+
+    situations_tree = build_tree(len(branches))
+    fill_tree(situations_tree, [])
+    situations_tree = consolidate_tree(situations_tree)
+    print(situations_tree)
+
+
 
 
 if __name__ == "__main__":
